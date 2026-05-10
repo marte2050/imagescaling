7f4984bc74f904f9d43cbdb209d3c414827e1efb
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2026-05-10 19:33:56 -0300

    feat: implement upload endpoint in ImagescalingController with file handling

--- apps/gateway/src/imagescaling/imagescaling.controller.ts
+++ apps/gateway/src/imagescaling/imagescaling.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ImagescalingService } from './imagescaling.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import type { uploadDTO } from './dto/uploadDTO';
 
 @Controller('imagescaling')
 export class ImagescalingController {
   constructor(private readonly imagescalingService: ImagescalingService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file: Express.Multer.File, @Body() information: uploadDTO) {
+    return this.imagescalingService.uploadImage(file, information);
+  }
 }
