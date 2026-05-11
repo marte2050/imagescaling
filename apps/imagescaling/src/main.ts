8b38ccc874d39ead46e5846697544120440cdcd3
Jonathan Willian de Oliveira <jonathan.oliveira@ifsuldeminas.edu.br>
2026-05-11 16:43:17 -0300

    feat: implement Kafka microservice for image scaling service

--- apps/imagescaling/src/main.ts
+++ apps/imagescaling/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'image-scaling-consumer',
+        },
+      },
+    },
+  );
+
+  await app.listen();
 }
-bootstrap();
+
+Logger.log('Image Scaling Service is running...');
+
+void bootstrap();
