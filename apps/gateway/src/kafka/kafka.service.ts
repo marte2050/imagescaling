77d78264c0d9d49d33de7d1ae7567f60709a51db
Jonathan Willian de Oliveira <jonathan.oliveira@ifsuldeminas.edu.br>
2026-05-14 13:16:22 -0300

    feat(kafka): update publishToKafka method to use uploadDTO for type safety

--- apps/gateway/src/kafka/kafka.service.ts
+++ apps/gateway/src/kafka/kafka.service.ts
@@ -1,12 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { KAFKA_SERVICE } from './kafka.module';
 import { ClientKafka } from '@nestjs/microservices';
+import { uploadDTO } from 'src/upload/dto/uploadDTO';
 
 @Injectable()
 export class KafkaService {
   constructor(@Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka) {}
 
-  publishToKafka(information: any, key: string) {
+  publishToKafka(information: uploadDTO, key: string) {
     this.kafkaClient.emit('image_uploaded', {
       url: key,
       ...information,
